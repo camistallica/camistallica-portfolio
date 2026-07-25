a802cdeac79f74af6819eacce653ab0ae4ed4373
camistallica <camilaferri108@gmail.com>
2026-07-24 21:04:54 -0300

    feat: create app_shell.dart

---
+++ camistallica_portfolio/lib/shell/app_shell.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../theme/nocturne_theme.dart';
+
+class AppShell {} extends StatelessWidget {
+  const AppShell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final n = context.n;
+
+    return Scaffold(
+      backgroundColor: n.bg,
+      body: const Center(
+        child: Text('shell'),
+      ),
+    );
+  }
+}
